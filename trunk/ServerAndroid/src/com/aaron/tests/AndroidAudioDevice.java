43395e32ca4a83bb257469819ecfe18badc7dc46
AaronAlbin@gmail.com <AaronAlbin@gmail.com@da6c9229-98a0-8d6c-932b-9dce217b3a3c>
2011-09-20 07:37:54 +0000

    adding server into serveranroid

---
+++ trunk/ServerAndroid/src/com/aaron/tests/AndroidAudioDevice.java
@@ -0,0 +1,36 @@
+package com.aaron.tests;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
+public class AndroidAudioDevice
+{
+   AudioTrack track;
+   short[] buffer = new short[1024];
+   int sr =44100;
+ 
+   public AndroidAudioDevice( )
+   {
+      int minSize =AudioTrack.getMinBufferSize( sr, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT );        
+      track = new AudioTrack( AudioManager.STREAM_MUSIC, sr, 
+                                        AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, 
+                                        minSize, AudioTrack.MODE_STREAM);
+      track.play();        
+   }	   
+ 
+   public void writeSamples(float[] samples) 
+   {	
+      fillBuffer( samples );
+      track.write( buffer, 0, samples.length );
+   }
+ 
+   private void fillBuffer( float[] samples )
+   {
+      if( buffer.length < samples.length )
+         buffer = new short[samples.length];
+ 
+      for( int i = 0; i < samples.length; i++ )
+         buffer[i] = (short)(samples[i] * Short.MAX_VALUE);;
+   }		
+}
