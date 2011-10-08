15182df25d76d6546936f632cf5a8121349b371b
AaronAlbin@gmail.com <AaronAlbin@gmail.com@da6c9229-98a0-8d6c-932b-9dce217b3a3c>
2011-10-08 00:30:15 +0000

    just adding some behavior code in here.. likely we will need to create a new thread or better way to handle this

--- trunk/Demokit2/src/com/google/android/DemoKit/ClientCode.java
+++ trunk/Demokit2/src/com/google/android/DemoKit/ClientCode.java
@@ -20,30 +20,30 @@ import android.widget.TextView;
 
 
 public class ClientCode implements OnClickListener{
-	
-	
+
+
 	boolean connected;
 	public String serverIpAddress;
 	public String line;
-	
+
 	BoeBotController bbc;
 	BeatTimer bt;
-	
+
 	Button connectToServer;
 	EditText serverIp;
-	
+
 	TextView fromServer;
-	
+
 	Handler handler = new Handler();
 	private DemoKitActivity mActivity;
-	
+
 	public ClientCode(DemoKitActivity mActivity, BoeBotController BBC)
 	{
 		bbc=BBC;
 		this.mActivity = mActivity;
 		attachToView();
 	}
-	
+
 	public void attachToView()
 	{
 		connectToServer = (Button) mActivity.findViewById(R.id.connectServer);
@@ -51,137 +51,139 @@ public class ClientCode implements OnClickListener{
 		serverIp = (EditText) mActivity.findViewById(R.id.serverIP);
 		fromServer=(TextView)mActivity.findViewById(R.id.textView1);
 	}
-	
-	
-	
+
+
+
 	///////////
 	public class ClientThread implements Runnable {
 		int myID=1;
 
 		public void run() {
-            try {
-                InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
-                Log.d("ClientActivity", "C: Connecting...");
-                Socket socket = new Socket(serverAddr, 8080);
-                connected = true;
-        		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        		PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+			try {
+				InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
+				Log.d("ClientActivity", "C: Connecting...");
+				Socket socket = new Socket(serverAddr, 8080);
+				connected = true;
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
-                while ((line = in.readLine()) != null) {
-				if(line.contains("header"))
-				{
-					
-				}
-				
-				//[ID]position:
-				//[ID]stop
-				//[ID]forward
-				//[ID]backward
-				if(line.contains("stop"+myID))
-				{
-					bbc.stop();
-					Log.d("LINE","stop");
-				}
-				if(line.contains("forward"+myID))
-				{
-					bbc.forward();
+				while ((line = in.readLine()) != null) {
+					if(line.contains("header"))
+					{
 
-					Log.d("LINE","forward");
-				}
-
-				if(line.contains("backward"+myID))
-				{
-					bbc.backward();
-
-					Log.d("LINE","backward");
-				}
-
-				if(line.contains("rotleft"+myID))
-				{
-					bbc.rotLeft();
-
-					Log.d("LINE","rotleft");
-				}
-
-				if(line.contains("rotright"+myID))
-				{
-					bbc.rotRight();
-
-					Log.d("LINE","rotright");
-				}
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						fromServer.setText("position: "+line);
 					}
-				});
 
-				//if it has myID
-				if(line.contains("position"))
-				{
-					String test [] = line.split(",");
+					//[ID]position:
+					//[ID]stop
+					//[ID]forward
+					//[ID]backward
+					if(line.contains("stop"+myID))
+					{
+						bbc.stop();
+						Log.d("LINE","stop");
+					}
+					if(line.contains("forward"+myID))
+					{
+						bbc.forward();
+
+						Log.d("LINE","forward");
+					}
+
+					if(line.contains("backward"+myID))
+					{
+						bbc.backward();
+
+						Log.d("LINE","backward");
+					}
+
+					if(line.contains("rotleft"+myID))
+					{
+						bbc.rotLeft();
+
+						Log.d("LINE","rotleft");
+					}
+
+					if(line.contains("rotright"+myID))
+					{
+						bbc.rotRight();
+
+						Log.d("LINE","rotright");
+					}
+					handler.post(new Runnable() {
+						@Override
+						public void run() {
+							fromServer.setText("position: "+line);
+						}
+					});
+
+					//if it has myID
+					if(line.contains("position"))
+					{
+
+						String test [] = line.split(",");
+						/*
 					Log.d("LINE", "test" + test.length);
 					Log.d("LINE", "test" + test[0]);
 					Log.d("LINE", "test" + test[1]);
 					Log.d("LINE", "test" + test[2]);
 					Log.d("LINE", "test" + test[3]);
+						 */
+						int ID = (int) Float.parseFloat(test[3]);
+						if(ID==myID)
+						{
+							int newx=(int) Float.parseFloat(    line.split(",")[1]       ) ;
+							int newy=(int) Float.parseFloat(    line.split(",")[2]  )    ;
 
-					int ID = Integer.parseInt(test[3]);
-					if(ID==myID)
-					{
-						int newx=(int) Float.parseFloat(    line.split(",")[1]       ) ;
-						int newy=(int) Float.parseFloat(    line.split(",")[2]  )    ;
-						
-						
-						
-						bbc.myvelx=bbc.myposx-newx;
-						bbc.myvely=bbc.myposy-newy;						
-						bbc.myangle = (float) Math.atan2(bbc.myvelx, bbc.myvely);
-						
-						bbc.myposx=(int) Float.parseFloat(    line.split(",")[1]       ) ;
-						bbc.myposy= (int) Float.parseFloat(    line.split(",")[2]  )    ;
-						
-						
-					}		
-					else
-					{
-						int newx=(int) Float.parseFloat(    line.split(",")[1]       ) ;
-						int newy=(int) Float.parseFloat(    line.split(",")[2]  )    ;
-						
-						bbc.targetvelx=bbc.targetx-newx;
-						bbc.targetvely=bbc.targety-newy;						
-						bbc.targetangle = (float) Math.atan2(bbc.targetvelx, bbc.targetvely);
-						
-						
-						bbc.targetx=(int) Float.parseFloat(    line.split(",")[1]       ) ;
-						bbc.targety=(int) Float.parseFloat(    line.split(",")[2]  )  ;
+
+
+							bbc.myvelx=bbc.myposx-newx;
+							bbc.myvely=bbc.myposy-newy;						
+							bbc.myangle = (float) Math.atan2(bbc.myvelx, bbc.myvely);
+
+							bbc.myposx=(int) Float.parseFloat(    line.split(",")[1]       ) ;
+							bbc.myposy= (int) Float.parseFloat(    line.split(",")[2]  )    ;
+
+
+						}		
+						else
+						{
+							int newx=(int) Float.parseFloat(    line.split(",")[1]       ) ;
+							int newy=(int) Float.parseFloat(    line.split(",")[2]  )    ;
+
+							bbc.targetvelx=bbc.targetx-newx;
+							bbc.targetvely=bbc.targety-newy;						
+							bbc.targetangle = (float) Math.atan2(bbc.targetvelx, bbc.targetvely);
+
+
+							bbc.targetx=(int) Float.parseFloat(    line.split(",")[1]       ) ;
+							bbc.targety=(int) Float.parseFloat(    line.split(",")[2]  )  ;
+						}
+
 					}
 
+					bbc.numberOfNeigbhors();
+
+					//Log.d("ClientActivity", line);
 				}
-
-				bbc.numberOfNeigbhors();
-
-    			Log.d("ClientActivity", line);
+			} catch (Exception e) {
+				Log.e("ClientActivity", "C: Error", e);
+				connected = false;
 			}
-            } catch (Exception e) {
-                Log.e("ClientActivity", "C: Error", e);
-                connected = false;
-            }
-        }
-    }
-	
+		}
+	}
+
 	/////////////
-	
-	
+
+
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+
 		if(v.getId()== connectToServer.getId())
 		{
-			 Log.d("ClientCode", "attempting to connect");
+			Log.d("ClientCode", "attempting to connect");
 			if (!connected) {
-			
+
 				serverIpAddress = serverIp.getText().toString();
 				if (!serverIpAddress.equals("")) {
 					Thread cThread = new Thread(new ClientThread());
@@ -189,8 +191,57 @@ public class ClientCode implements OnClickListener{
 				}
 			}
 		}
-	
-		
+
+
+	}
+
+
+	public class Behavior
+	{
+		public Behavior()
+		{
+
+		}
+
+
+		void wander()
+		{
+
+			//move forward for sometime by default..
+
+
+
+
+			// if reach boundary
+			if(boundaryReached() )
+			{
+				int xbound=10;
+				if(bbc.myposx < 0 + xbound)
+				{
+					//get what current angle is
+					if(bbc.myangle>90 || bbc.myangle <270)
+					{
+						//this should probably be rotate until something
+						bbc.rotLeft();
+					}
+					else
+					{
+
+						//rotate until somecondition
+						bbc.rotRight();
+					}
+				}
+			}
+
+		}
+
+		boolean boundaryReached()
+		{
+
+
+			return true;
+		}
+
 	}
 
 }
