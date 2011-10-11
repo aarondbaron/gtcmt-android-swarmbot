b62552c148f7d87c93bb239aca8fc7e291d0913d
mostbestjia <mostbestjia@da6c9229-98a0-8d6c-932b-9dce217b3a3c>
2011-10-11 22:07:20 +0000

    Bot class Behavior class

--- trunk/Demokit2/src/com/google/android/DemoKit/BoeBotController.java
+++ trunk/Demokit2/src/com/google/android/DemoKit/BoeBotController.java
@@ -94,7 +94,7 @@ public class BoeBotController implements OnClickListener, SensorEventListener
 	
 	public float  angleAzimuth;
 	
-	Vector otherBots;
+	Vector<Bot> otherBots;
 
 	public BoeBotController(DemoKitActivity activity, int servo1, int servo2) {
 		mActivity = activity;
@@ -1213,55 +1213,6 @@ public class BoeBotController implements OnClickListener, SensorEventListener
     	}
 
 		 
-	}
-
-	// a class for keeping track of other bots
-	public class Bot
-	{
-		int x, y;
-		int vx, vy;
-		float angle;
-
-		int ID;
-
-		Bot()
-		{
-
-		}
-
-		Bot(int x, int y)
-		{
-		}
-
-		int[] getPos()
-		{	
-			return new int[]{x,y};
-		}
-		void setPos(int x, int y)
-		{
-			this.x=x;
-			this.y=y;
-		}
-		int[] getVel()
-		{
-			return new int[]{vx,vy};
-		}
-		void setVel(int vx, int vy)
-		{
-			this.vx=vx;
-			this.vy=vy;
-
-			angle = (float) Math.atan2(vy, vx);
-
-		}
-
-
-
-
-
-
-
-
 	}
 
 
---
+++ trunk/Demokit2/src/com/google/android/DemoKit/Bot.java
@@ -0,0 +1,44 @@
+package com.google.android.DemoKit;
+
+// a class for keeping track of other bots
+public class Bot
+{
+	int x, y;
+	int vx, vy;
+	float angle;
+
+	int ID;
+
+	Bot()
+	{
+
+	}
+
+	Bot(int x, int y)
+	{
+	}
+
+	int[] getPos()
+	{	
+		return new int[]{x,y};
+	}
+	void setPos(int x, int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	int[] getVel()
+	{
+		return new int[]{vx,vy};
+	}
+	void setVel(int vx, int vy)
+	{
+		this.vx=vx;
+		this.vy=vy;
+
+		angle = (float) Math.atan2(vy, vx);
+
+	}
+
+}
+
