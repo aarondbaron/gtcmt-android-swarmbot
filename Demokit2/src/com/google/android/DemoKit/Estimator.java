77ea7466f11de2caee5a58a9909a49581d130d34
AaronAlbin@gmail.com <AaronAlbin@gmail.com@da6c9229-98a0-8d6c-932b-9dce217b3a3c>
2012-02-11 06:03:15 +0000

    somethign to eventually keep track of all things being estimated

---
+++ Demokit2/src/com/google/android/DemoKit/Estimator.java
@@ -0,0 +1,50 @@
+package com.google.android.DemoKit;
+
+public class Estimator {
+	
+	PVector v;
+	float ang;
+	
+	int n;
+	
+	int[] px,py;
+	
+	int iter=0;
+	
+	long interval;
+	
+	BoeBotController bbc;
+	
+	Estimator(BoeBotController bbc)
+	{
+		this.bbc=bbc;
+		interval = System.currentTimeMillis();
+		v = new PVector();
+		
+		n=10;
+		
+		px= new int[n];
+		py= new int[n];
+		
+		
+	}
+	
+	float getAngle()
+	{
+		
+		//source 1 -- camera position
+		// source 2 -- camera angle
+		// source 3 --  compass
+		float source1=v.heading2D()+(float)Math.PI;
+		float source2=source1;
+		float source3=source1;
+		
+		ang=.5f*(source1) + .5f*(source2) + .5f*source3;
+		
+		return ang;
+	}
+	
+	
+	
+
+}
